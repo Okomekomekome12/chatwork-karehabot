cdcb4337f514de57019ce2b105feb9f3864ee0c6
Okomekomekome2 <gyuti@dakaka.org>
2026-03-04 20:33:15 +0900

    new file:   Procfile 	new file:   __init__.py 	new file:   chatwork.py 	new file:   main.py 	new file:   requirements.txt

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import time
+import chatwork
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+API_TOKEN = "9f89821d6eac0bba7adb611b00fc164e"
+SECRET_TOKEN = None
+
+@app.route("/", methods=["GET"])
+def health():
+    return "OK"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    signature = request.headers.get("X-ChatWorkWebhookSignature")
+    if not chatwork.webhook_verify_signature(request.data, signature, SECRET_TOKEN): # type: ignore
+        return "invalid signature", 403
+    data = request.json
+    room_id    = chatwork.webhook_get_roomid(data) # type: ignore
+    body       = chatwork.webhook_get_message(data)
+    account_id = chatwork.webhook_get_account_id(data) # type: ignore
+    message_id = chatwork.webhook_get_message_id(data)
+
+
+    #ーーーーーーーーーーーーーーーーーーーーーーーーーーーメインコードーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+    cw = chatwork.setup(room_id, API_TOKEN)
+    if body == "hello world":
+        cw.messagesend("hello!")
+
+    return jsonify({"status": "ok"}), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
