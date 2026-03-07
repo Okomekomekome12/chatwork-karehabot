86d117e9e2f1b4d6a5082c864dde2d8cf5106642
Okomekomekome2 <gyuti@dakaka.org>
2026-03-07 20:17:55 +0900

    modified:   modules/math.py

--- modules/math.py
+++ modules/math.py
@@ -10,9 +10,9 @@ second = random.randint(1,100)
 def start(cw):
     calculation = random.choice(["plus","minus","multiplication","division"])
     if calculation == "plus":
-        cw.messagesend(f"{first} + {second} = ? \n5秒以内で答えてください")
+        cw.messagesend(f"{first} + {second} = ? \n10秒以内で答えてください")
         answer = first + second
-        time.sleep(5)
+        time.sleep(10)
         messages = cw.get_new_messages()
         if messages == answer:
             cw.messagesend("正解です")
@@ -20,9 +20,9 @@ def start(cw):
             cw.messagesend("不正解です")
         return answer
     elif calculation == "minus":
-        cw.messagesend(f"{first} - {second} = ?\n5秒以内に答えてください")
+        cw.messagesend(f"{first} - {second} = ?\n10秒以内に答えてください")
         answer = first - second
-        time.sleep(5)
+        time.sleep(10)
         messages = cw.get_new_messages()
         if messages == answer:
             cw.messagesend("正解です")
