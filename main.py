f5064c94a306a0de1ab021e9a724778ab782fb82
Okomekomekome12 <okomekomekome1@outlook.com>
2026-03-08 18:34:51 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ def webhook():
     if body == "/startmath":
         answer = math.start(cw)
         cw.messagesend(f"{answer}")
+    elif body == "/help":
+        cw.messagesend("[info][title]これはカレハbotのコマンド一覧だよ、よく読め([/title][info]/startmath : 簡単()な計算問題を出してきます、15秒待って最後に取得したメッセージが数字ではないとエラーを吐いて止まる親切設計です()[/info][hr]/help : ただのヘルプやで[/info]")
 
     return jsonify({"status": "ok"}), 200
 
