ebac2b14c5dcc5fff6d7d3659e6eebbe98fa3f24
Okomekomekome12 <okomekomekome1@outlook.com>
2026-03-23 14:47:58 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def webhook():
         return jsonify({"status": "ok"}), 200
     if body == "/startmath":  # ← "/startmath" と完全一致
         math.start(account_id, cw)
-    if account_id == 11156582:
+    if int(account_id) == 11156582:
         return jsonify({"status": "ok"}), 200
     if account_id in math.current_questions:
         math.answer(account_id, body, cw)
