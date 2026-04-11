85dc09f473b3090d307f73977c53c932c1a8c3c9
Okomekomekome12 <okomekomekome1@outlook.com>
2026-04-11 19:49:15 +0900

    modified:   index.py

--- index.py
+++ index.py
@@ -53,7 +53,8 @@ def webhook():
 
     chatwork.auto_accept_contacts(API_TOKEN)
     # 自分のメッセージは無視
-
+    if body == "/readme":
+        cw.messagesend("このbotを導入したいと思ったことはありますよねぇ！？そうですよねぇ！？（圧）\n")
     if int(account_id) == BOT_ACCOUNT_ID:
         print("→ Bot自身のメッセージなのでスキップ")
         return jsonify({"status": "ok"}), 200
@@ -101,6 +102,7 @@ def webhook():
 [info]/delete-rammerhead /delete-utopia /delete-wakame /delete-other : リスト一覧から削除します[/info]\
 [hr]\
 [info]/link: リンク一覧を表示します[/info]\
+[info]/readme : 読　ん　で　ね[/info]\
 [/info]")
     elif body == "/update":
         cw.messagesend("[info][title]アップデート情報[/title]\
