f469febb2440bd42f5e6947bde6746f84a5fd39d
Okomekomekome12 <okomekomekome1@outlook.com>
2026-05-02 20:10:45 +0900

    modified:   modules/help.py

--- modules/help.py
+++ modules/help.py
@@ -1,22 +1,22 @@
 def help(cw,account_id,room_id,message_id):
     cw.messagesend(f"[rp aid={account_id} to={room_id}-{message_id}][pname:{account_id}]さん\
     [info][title]これはカレハbotのコマンド一覧だよ、よく読め([/title]\
-    [info][info]/startmath : 簡単()な計算問題を出してきます[/info][/info]\
+    [info][code]/startmath : 簡単()な計算問題を出してきます[/code][/info]\
     [hr]\
-    [info][info]/助けて : cwbotとコマンド重なるのでこれ()[/info][/info]\
+    [info][code]/助けて : cwbotとコマンド重なるのでこれ()[/code][/info]\
     [hr]\
-    [info][info]/update : アップデートしたら乗せるお[/info][/info]\
+    [info][code]/update : アップデートしたら乗せるお[/code][/info]\
     [hr]\
-    [info][info]/add-rammerhead /add-utopia /add-wakame /add-other : リスト一覧に追加します[/info][/info]\
+    [info][code]/add-rammerhead /add-utopia /add-wakame /add-other : リスト一覧に追加します[/code][/info]\
     [hr]\
-    [info][info]/delete-rammerhead /delete-utopia /delete-wakame /delete-other : リスト一覧から削除します[/info][/info]\
+    [info][code]/delete-rammerhead /delete-utopia /delete-wakame /delete-other : リスト一覧から削除します[/code][/info]\
     [hr]\
-    [info][info]/link: リンク一覧を表示します[/info][/info]\
+    [info][code]/link: リンク一覧を表示します[/code][/info]\
     [hr]\
-    [info][info]/live? : 荒らし対策botの稼働確認です[/info][/info]\
+    [info][code]/live? : 荒らし対策botの稼働確認です[/code][/info]\
     [hr]\
-    [info][info]/readme : 読　ん　で　ね[/info][/info]\
-    [info][info]botが送信したメッセージに対して削除と返信 : メッセージが消えます[/info][/info]\
+    [info][code]/readme : 読　ん　で　ね[/code][/info]\
+    [info][code]botが送信したメッセージに対して削除と返信 : メッセージが消えます[/code][/info]\
     [hr]\
 \n\n\
     [hr]\
