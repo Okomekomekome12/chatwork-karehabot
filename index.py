e5fbcd80cf8f028e2fe6c7b73009ddf07914fc6c
Okomekomekome12 <okomekomekome1@outlook.com>
2026-05-04 17:25:01 +0900

    modified:   index.py

--- index.py
+++ index.py
@@ -39,164 +39,166 @@ def webhook():
     cw = chatwork.setup(room_id, API_TOKEN)
     cw2 = chatwork.setup(420107748,API_TOKEN)
     log_room = chatwork.setup(418992889,API_TOKEN)
+    try:
+        if int(account_id) == BOT_ACCOUNT_ID:
+            print("→ Bot自身のメッセージなのでスキップ")
+            return jsonify({"status": "ok"}), 200
 
-    if int(account_id) == BOT_ACCOUNT_ID:
-        print("→ Bot自身のメッセージなのでスキップ")
-        return jsonify({"status": "ok"}), 200
+        logs = body.replace("[/code]","")
+        log_room.messagesend(f"[info][title][piconname:{account_id}]のメッセージ \nメッセリンク→https://www.chatwork.com/#!rid{room_id}-{message_id}[/title][code]{logs}[/code][/info]")
 
-    logs = body.replace("[/code]","")
-    log_room.messagesend(f"[info][title][piconname:{account_id}]のメッセージ \nメッセリンク→https://www.chatwork.com/#!rid{room_id}-{message_id}[/title][code]{logs}[/code][/info]")
+        if body == "/start":
+            cw.messagesend("起動します")
+            shutdown = False
+            return jsonify({"status": "ok"}), 200
+        
+        if shutdown == True:
+            print("シャットダウン中なのでスキップ")
+            return jsonify({"status": "ok"}), 200
+        
+        if body.find("[dtext:chatroom_member_is]") == 0:
+            target_account_id = body[38:47]
+            print(target_account_id)
+            blacklist.check(cw,target_account_id)
+            
 
-    if body == "/start":
-        cw.messagesend("起動します")
-        shutdown = False
-        return jsonify({"status": "ok"}), 200
-    
-    if shutdown == True:
-        print("シャットダウン中なのでスキップ")
-        return jsonify({"status": "ok"}), 200
-    
-    if body.find("[dtext:chatroom_member_is]") == 0:
-        target_account_id = body[38:47]
-        print(target_account_id)
-        blacklist.check(cw,target_account_id)
+        if body and (body.count("(quick)") >= 10 or body.count(":*") >= 10):
+            cw.viewer(account_id)
+            cw.messagesend("[info][title]荒らし検知[/title]荒らしを検知しました、流します[/info]")
+
+            for i in range(70):
+                cw.messagesend("a")
+                time.sleep(0.7)
+
+            message_link = cw.get_message_link()
+            cw.messagesend("[info][title]荒らし対処完了[/title]寝の部屋にメッセージリンクを配布します[/info]")
+            cw2.messagesend(f"[info][title]メッセリンク配布[/title]{message_link}[/info]")
+
+            description = log_room.get_description()
+            room_name = log_room.get_room_name()
+            log_room.edit_room_description(f"[info][title]メッセリンク配布[/title]{message_link}[/info]" + str(description),room_name)
+            
+            blacklist.add(account_id)
+
+        if body and body.count("[toall]") >= 1:
+            cw.viewer(account_id)
+            cw.messagesend("[info][title]toall検知[/title]何してんねんハゲぇぇぇぇぇぇぇぇ（（（[/info]")
+
+        if body == "/live?":
+            cw.messagesend("[info][title]荒らし対策bot正常稼働中[/title]生きてるお[/info]")
+
+        
+        chatwork.auto_accept_contacts(API_TOKEN) # コンタクト承認
+
+        # 自分のメッセージは無視
+        if body == "/readme":
+            cw.messagesend("このbotを導入したいと思ったことはありますよねぇ！？そうですよねぇ！？（圧）\n")
+
+
+        # 計算問題の回答チェック
+        if account_id in math.current_questions:
+            print(f"→ 計算問題の回答チェック実行")
+            math.answer(account_id, body, cw)
+            return jsonify({"status": "ok"}), 200
+
+        if body == "/shutdown" and account_id == 10870480:
+            cw.messagesend("シャットダウンします、、、")
+            shutdown = True
+
+        elif body == "/shutdown":
+            cw.messagesend("この操作は古米しかできないお")
+
+        if body and body.count("削除") >= 1:
+            target = body.split("to=")[1].split("]")[0]  
+            delete_room_id , delete_message_id = target.split("-")
+            deleter_room_id = delete_room_id
+            deleter_message_id = delete_message_id
+            print(deleter_room_id,deleter_message_id)
+            cw.delete_message(deleter_message_id)
+
+        # URL待ち状態の処理
+        if account_id in user_state:
+            state = user_state.pop(account_id)
+            if state == "add-rammerhead":
+                add_url.add_rammerhead(body, cw)
+            elif state == "add-utopia":
+                add_url.add_utopia(body, cw)
+            elif state == "add-wakame":
+                add_url.add_wakame(body, cw)
+            elif state == "add-other":
+                add_url.add_other(body, cw)
+            elif state == "delete-rammerhead":
+                add_url.delete_rammerhead(body, cw)
+            elif state == "delete-utopia":
+                add_url.delete_utopia(body, cw)
+            elif state == "delete-wakame":
+                add_url.delete_wakame(body, cw)
+            elif state == "delete-other":
+                add_url.delete_other(body, cw)
+            return jsonify({"status": "ok"}), 200
         
 
-    if body and (body.count("(quick)") >= 10 or body.count(":*") >= 10):
-        cw.viewer(account_id)
-        cw.messagesend("[info][title]荒らし検知[/title]荒らしを検知しました、流します[/info]")
-
-        for i in range(70):
-            cw.messagesend("a")
-            time.sleep(0.7)
-
-        message_link = cw.get_message_link()
-        cw.messagesend("[info][title]荒らし対処完了[/title]寝の部屋にメッセージリンクを配布します[/info]")
-        cw2.messagesend(f"[info][title]メッセリンク配布[/title]{message_link}[/info]")
-
-        description = log_room.get_description()
-        room_name = log_room.get_room_name()
-        log_room.edit_room_description(f"[info][title]メッセリンク配布[/title]{message_link}[/info]" + str(description),room_name)
+        if body == "/startmath":
+            print(f"→ /startmath 実行")
+            math.start(account_id, cw)
         
-        blacklist.add(account_id)
+        if body == "/助けて":
+            help.help(cw,account_id,room_id,message_id)
 
-    if body and body.count("[toall]") >= 1:
-        cw.viewer(account_id)
-        cw.messagesend("[info][title]toall検知[/title]何してんねんハゲぇぇぇぇぇぇぇぇ（（（[/info]")
-
-    if body == "/live?":
-        cw.messagesend("[info][title]荒らし対策bot正常稼働中[/title]生きてるお[/info]")
-
-    
-    chatwork.auto_accept_contacts(API_TOKEN) # コンタクト承認
-
-    # 自分のメッセージは無視
-    if body == "/readme":
-        cw.messagesend("このbotを導入したいと思ったことはありますよねぇ！？そうですよねぇ！？（圧）\n")
-
-
-    # 計算問題の回答チェック
-    if account_id in math.current_questions:
-        print(f"→ 計算問題の回答チェック実行")
-        math.answer(account_id, body, cw)
-        return jsonify({"status": "ok"}), 200
-
-    if body == "/shutdown" and account_id == 10870480:
-        cw.messagesend("シャットダウンします、、、")
-        shutdown = True
-
-    elif body == "/shutdown":
-        cw.messagesend("この操作は古米しかできないお")
-
-    if body and body.count("削除") >= 1:
-        target = body.split("to=")[1].split("]")[0]  
-        delete_room_id , delete_message_id = target.split("-")
-        deleter_room_id = delete_room_id
-        deleter_message_id = delete_message_id
-        print(deleter_room_id,deleter_message_id)
-        cw.delete_message(deleter_message_id)
-
-    # URL待ち状態の処理
-    if account_id in user_state:
-        state = user_state.pop(account_id)
-        if state == "add-rammerhead":
-            add_url.add_rammerhead(body, cw)
-        elif state == "add-utopia":
-            add_url.add_utopia(body, cw)
-        elif state == "add-wakame":
-            add_url.add_wakame(body, cw)
-        elif state == "add-other":
-            add_url.add_other(body, cw)
-        elif state == "delete-rammerhead":
-            add_url.delete_rammerhead(body, cw)
-        elif state == "delete-utopia":
-            add_url.delete_utopia(body, cw)
-        elif state == "delete-wakame":
-            add_url.delete_wakame(body, cw)
-        elif state == "delete-other":
-            add_url.delete_other(body, cw)
-        return jsonify({"status": "ok"}), 200
-    
-
-    if body == "/startmath":
-        print(f"→ /startmath 実行")
-        math.start(account_id, cw)
-    
-    if body == "/助けて":
-        help.help(cw,account_id,room_id,message_id)
-
-    if body == "/update":
-        cw.messagesend("[info][title]アップデート情報[/title]\
-ytdlpコマンド追加！仙人さんコード提供ありがとうございます！[/info]")
+        if body == "/update":
+            cw.messagesend("[info][title]アップデート情報[/title]\
+    ytdlpコマンド追加！仙人さんコード提供ありがとうございます！[/info]")
+            
+        if body == "/link":
+            add_url.show_list(cw)
         
-    if body == "/link":
-        add_url.show_list(cw)
-    
-    if body == "/ytdlp":
-        ytdlp_check.check_status(cw)
+        if body == "/ytdlp":
+            ytdlp_check.check_status(cw)
 
-    if body == "/blacklist":
-        blacklist.accont_list(cw)
-    
-    if body.find("/delete-blacklist") == 0:
-        account_id = body.split()[1]
-        blacklist.delete(cw,account_id)
+        if body == "/blacklist":
+            blacklist.accont_list(cw)
+        
+        if body.find("/delete-blacklist") == 0:
+            account_id = body.split()[1]
+            blacklist.delete(cw,account_id)
 
 
-    if body == "/add-rammerhead":
-        cw.messagesend("ランマーヘッドをリスト一覧に追加します\nこのメッセージの次に\"必ず\"リンクを載せてください")
-        user_state[account_id] = "add-rammerhead"
-    
-    elif body == "/add-utopia":
-        cw.messagesend("utopiaをリスト一覧に追加します\nこのメッセージの次に\"必ず\"リンクを載せてください")
-        user_state[account_id] = "add-utopia"
-    
-    elif body == "/add-wakame":
-        cw.messagesend("わかめtubeをリスト一覧に追加します\nこのメッセージの次に\"必ず\"リンクを載せてください")
-        user_state[account_id] = "add-wakame"
-    
-    elif body == "/add-other":
-        cw.messagesend("その他をリスト一覧に追加します\nこのメッセージの次に\"必ず\"リンクを載せてください")
-        user_state[account_id] = "add-other"
-    
-    elif body == "/delete-rammerhead":
-        cw.messagesend("ランマーヘッドをリスト一覧から削除します\nこのメッセージの次に\"必ず\"リンクを載せてください")
-        user_state[account_id] = "delete-rammerhead"
-    
-    elif body == "/delete-utopia":
-        cw.messagesend("utopiaをリスト一覧から削除します\nこのメッセージの次に\"必ず\"リンクを載せてください")
-        user_state[account_id] = "delete-utopia"
-    
-    elif body == "/delete-wakame":
-        cw.messagesend("わかめtubeをリスト一覧から削除します\nこのメッセージの次に\"必ず\"リンクを載せてください")
-        user_state[account_id] = "delete-wakame"
-    
-    elif body == "/delete-other":
-        cw.messagesend("その他をリスト一覧から削除します\nこのメッセージの次に\"必ず\"リンクを載せてください")
-        user_state[account_id] = "delete-other"
-    
-    return jsonify({"status": "ok"}), 200
-
+        if body == "/add-rammerhead":
+            cw.messagesend("ランマーヘッドをリスト一覧に追加します\nこのメッセージの次に\"必ず\"リンクを載せてください")
+            user_state[account_id] = "add-rammerhead"
+        
+        elif body == "/add-utopia":
+            cw.messagesend("utopiaをリスト一覧に追加します\nこのメッセージの次に\"必ず\"リンクを載せてください")
+            user_state[account_id] = "add-utopia"
+        
+        elif body == "/add-wakame":
+            cw.messagesend("わかめtubeをリスト一覧に追加します\nこのメッセージの次に\"必ず\"リンクを載せてください")
+            user_state[account_id] = "add-wakame"
+        
+        elif body == "/add-other":
+            cw.messagesend("その他をリスト一覧に追加します\nこのメッセージの次に\"必ず\"リンクを載せてください")
+            user_state[account_id] = "add-other"
+        
+        elif body == "/delete-rammerhead":
+            cw.messagesend("ランマーヘッドをリスト一覧から削除します\nこのメッセージの次に\"必ず\"リンクを載せてください")
+            user_state[account_id] = "delete-rammerhead"
+        
+        elif body == "/delete-utopia":
+            cw.messagesend("utopiaをリスト一覧から削除します\nこのメッセージの次に\"必ず\"リンクを載せてください")
+            user_state[account_id] = "delete-utopia"
+        
+        elif body == "/delete-wakame":
+            cw.messagesend("わかめtubeをリスト一覧から削除します\nこのメッセージの次に\"必ず\"リンクを載せてください")
+            user_state[account_id] = "delete-wakame"
+        
+        elif body == "/delete-other":
+            cw.messagesend("その他をリスト一覧から削除します\nこのメッセージの次に\"必ず\"リンクを載せてください")
+            user_state[account_id] = "delete-other"
+        
+        return jsonify({"status": "ok"}), 200
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+        return jsonify({"status": "ok"}), 200
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
