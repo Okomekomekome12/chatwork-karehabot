15a8242e976c24ff044bc03ba3649562cead7ce9
Okomekomekome12 <okomekomekome1@outlook.com>
2026-05-04 17:08:06 +0900

    modified:   modules/help.py

--- modules/help.py
+++ modules/help.py
@@ -1,29 +1,29 @@
 def help(cw,account_id,room_id,message_id):
     cw.messagesend(f"[rp aid={account_id} to={room_id}-{message_id}][pname:{account_id}]さん\
-    [info][title]これはカレハbotのコマンド一覧だよ、よく読め([/title]\
-    [info][code]/startmath : 簡単()な計算問題を出してきます[/code][/info]\
-    [hr]\
-    [info][code]/助けて : cwbotとコマンド重なるのでこれ()[/code][/info]\
-    [hr]\
-    [info][code]/update : アップデートしたら乗せるお[/code][/info]\
-    [hr]\
-    [info][code]/add-rammerhead /add-utopia /add-wakame /add-other : リスト一覧に追加します[/code][/info]\
-    [hr]\
-    [info][code]/delete-rammerhead /delete-utopia /delete-wakame /delete-other : リスト一覧から削除します[/code][/info]\
-    [hr]\
-    [info][code]/link: リンク一覧を表示します[/code][/info]\
-    [hr]\
-    [info][code]/live? : 荒らし対策botの稼働確認です[/code][/info]\
-    [hr]\
-    [info][code]/readme : 読　ん　で　ね[/code][/info]\
-    [info][code]/ytdlp : Ytdlpの稼働状況を表示します[/code][/info]\
-    [hr]\
-    [info][code]/blacklist : ブラックリスト一覧を表示します[/code][/info]\
-    [hr]\
-    [info][code]/delete-blacklist {account_id}: ブラックリストから削除します\n例: /delete-blacklist 1919114514[/code][/info]\
-    [hr]\
-    [info][code]botが送信したメッセージに対して削除と返信 : メッセージが消えます[/code][/info]\
-    [hr]\
-    メッセージリンク配布所→https://www.chatwork.com/g/messageLink\
-    [/info]"
+[info][title]これはカレハbotのコマンド一覧だよ、よく読め([/title]\
+[info][code]/startmath : 簡単()な計算問題を出してきます[/code][/info]\
+[hr]\
+[info][code]/助けて : cwbotとコマンド重なるのでこれ()[/code][/info]\
+[hr]\
+[info][code]/update : アップデートしたら乗せるお[/code][/info]\
+[hr]\
+[info][code]/add-rammerhead /add-utopia /add-wakame /add-other : リスト一覧に追加します[/code][/info]\
+[hr]\
+[info][code]/delete-rammerhead /delete-utopia /delete-wakame /delete-other : リスト一覧から削除します[/code][/info]\
+[hr]\
+[info][code]/link: リンク一覧を表示します[/code][/info]\
+[hr]\
+[info][code]/live? : 荒らし対策botの稼働確認です[/code][/info]\
+[hr]\
+[info][code]/readme : 読　ん　で　ね[/code][/info]\
+[info][code]/ytdlp : Ytdlpの稼働状況を表示します[/code][/info]\
+[hr]\
+[info][code]/blacklist : ブラックリスト一覧を表示します[/code][/info]\
+[hr]\
+[info][code]/delete-blacklist {account_id}: ブラックリストから削除します\n例: /delete-blacklist 1919114514[/code][/info]\
+[hr]\
+[info][code]botが送信したメッセージに対して削除と返信 : メッセージが消えます[/code][/info]\
+[hr]\
+メッセージリンク配布所→https://www.chatwork.com/g/messageLink\
+[/info]"
     )
