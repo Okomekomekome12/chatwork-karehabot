9c20b08f00d6322942d1494ad74ad40d2342e114
Okomekomekome2 <gyuti@dakaka.org>
2026-05-19 14:42:01 +0900

    modified:   index.py

--- index.py
+++ index.py
@@ -54,6 +54,7 @@ def webhook():
     print(f"AI_flag: {AI_flag}")
     print(f"AI_room_id: {AI_room_id}")
     print(f"AI_second_id: {AI_second_id}")
+    print(f"AI_count: {AI_count}")
     print(f"==================\n")
     cw = chatwork.setup(room_id, API_TOKEN)
     cw2 = chatwork.setup(420107748,API_TOKEN)
