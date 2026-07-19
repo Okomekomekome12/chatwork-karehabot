43909d548752ab4f24d7fd94f5b803688bbded72
Okomekomekome12 <okomekomekome1@outlook.com>
2026-07-19 12:04:43 +0900

    blacklistを修正

--- modules_blacklist.py
+++ modules_blacklist.py
@@ -4,10 +4,10 @@ SUPABASE_URL = "https://xzphpphzzbffrdwuzgir.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6cGhwcGh6emJmZnJkd3V6Z2lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwODAxOTIsImV4cCI6MjA4OTY1NjE5Mn0.L9c5Uz2zB6AZ8joMd1YdsgzxxcVpfiEwvenkbR_qtBE"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-def add(cw,account_id):
+def add(account_id):
     data = {"account_id": account_id}
     result = supabase.table("accounts").insert(data).execute()
-    cw.messagesend(f"account_id保存完了 : {account_id}")
+    print(f"account_id保存完了 : {account_id}")
     return result
 
 def check(cw,account_id):
