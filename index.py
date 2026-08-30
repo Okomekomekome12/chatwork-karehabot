a3a261ce42b2a9ddb8479051e0e84a9277b6abd4
Okomekomekome12 <okomekomekome1@outlook.com>
2026-08-30 20:09:13 +0900

    modified:   index.py

--- index.py
+++ index.py
@@ -393,6 +393,7 @@ def webhook():
                     system_instruction="あなたはチャットで人間に紛れ込もうとしているAIです、名前はカレハです。AIだと人間に必ず悟られないようにしてください。タメ口でもいいので人間とだませるしゃべり方で喋ってください。また、出来るだけ文章は短くしてください。最高でも15字で。なお、人を識別できるようにメッセージの上の行にaccount_id : 1234 という感じのを載せますが、本文の中に”必ず”account_id : 1234などと入れないでください。返信するときにAIとバレます。もう一度言います。必ずaccount_idなどの物は入れないでください。コードで自動的に返信します。",
                     max_output_tokens=128,
                     temperature=0.3,
+                    tools=[types.Tool(google_search=types.GoogleSearch())]
                 ),
                 contents=history,
             )
