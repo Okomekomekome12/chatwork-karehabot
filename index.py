e9ec7d5b5afa54f68bdf9ef2d202bda674dc3e8d
Okomekomekome12 <okomekomekome1@outlook.com>
2026-09-21 20:24:00 +0900

    modified:   index.py

--- index.py
+++ index.py
@@ -88,6 +88,8 @@ def webhook():
     print(f"gemini_account_id: {gemini_account_id}")
     print(body[86:94])
     print(body.find("[dtext:chatroom_added]"))
+    print("\n")
+    print("remote_addr:", request.remote_addr)
     print(f"==================\n")
 
     cw       = chatwork.setup(room_id, API_TOKEN)
