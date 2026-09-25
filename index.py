56ffda7997e3956262f48aeee23006c5bb169a85
Okomekomekome12 <okomekomekome1@outlook.com>
2026-09-26 07:29:42 +0900

    modified:   __pycache__/chatwork_no_api.cpython-313.pyc 	modified:   index.py

--- index.py
+++ index.py
@@ -48,7 +48,7 @@ def webhook():
     signature = request.headers.get("X-ChatWorkWebhookSignature")
     if not chatwork.webhook_verify_signature(request.data, signature, SECRET_TOKEN): # type: ignore
         return "invalid signature", 403
-    ip         = request.remote_addr
+    ip         = request.headers.get('X-Forwarded-For').split(',')[0].strip()
     data       = request.json
     room_id    = chatwork.webhook_get_roomid(data)
     body       = chatwork.webhook_get_message(data)
@@ -89,22 +89,22 @@ def webhook():
     print(body[86:94])
     print(body.find("[dtext:chatroom_added]"))
     print("\n")
-    print("remote_addr:", request.remote_addr)
+    print("ip:", ip)
     print(f"==================\n")
 
 
 
-    cw       = chatwork.setup(room_id, API_TOKEN)
+    cw          = chatwork.setup(room_id, API_TOKEN)
     #no_api_log = chatwork_no_api.setup(418992889 , BOT_ACCOUNT_ID , ssid)
-    cw2      = chatwork.setup(420107748,API_TOKEN)
-    log_room = chatwork.setup(418992889,API_TOKEN)
+    cw2         = chatwork.setup(420107748,API_TOKEN)
+    log_room    = chatwork.setup(418992889,API_TOKEN)
+    error_room  =chatwork.setup(448675989,API_TOKEN)
     role     = cw.is_admin(account_id)
     print(role)
     try:
-        if ip == "10.0.4.129":
-            pass
+        """
         else:
-            return jsonify({"status" : "ok"}), 200
+            return jsonify({"status" : "ok"}), 200"""
         if int(account_id) == BOT_ACCOUNT_ID:
             print("→ Bot自身のメッセージなのでスキップ")
             return jsonify({"status": "ok"}), 200
@@ -487,7 +487,7 @@ def webhook():
         return jsonify({"status": "ok"}), 200
     except Exception as e:
         print(f"エラーが発生しました: {e}")
-        #cw.messagesend(f"エラーが発生しました: {str(e)[:10]}")
+        error_room.messagesend(f"[info][title]エラー発生[hr]対象部屋 : {room_id}[/title][code]{e}[/code][/info]")
         return jsonify({"status": "ok"}), 200
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
